1d015b1f2875792d85b3d994cb8a0359e83a6e7d
zhaoyandong <1786368526@qq.com>
2025-01-14 10:03:58 +0800

    feat: 增加画图功能

--- tools.py
+++ tools.py
@@ -21,16 +21,13 @@ def analyze_results(adj_matrix, labels):
         nmi: float, 标准化互信息
         ari: float, 调整兰德系数
     """
-
-    
     # 将邻接矩阵转换为NetworkX图
     G = nx.from_numpy_array(adj_matrix)
     
     # 使用Louvain算法进行社团检测
-    communities = community_louvain.best_partition(G, resolution=2.0)
-    
+    communities = community_louvain.best_partition(G, weight='weight', resolution=2.0)
     # 计算模块度
-    modularity = community_louvain.modularity(communities, G)
+    modularity = community_louvain.modularity(communities, G, weight='weight')
     
     # 获取社团标签列表
     pred_labels = [communities[i] for i in range(len(communities))]
@@ -40,3 +37,6 @@ def analyze_results(adj_matrix, labels):
     ari = adjusted_rand_score(labels, pred_labels)
 
     return communities, modularity, nmi, ari
+
+
+
